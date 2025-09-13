a6850f7873c41f0daf52342f16d440fa87158b08
AksTraL <akshat25chauhan.2005@gmail.com>
2025-09-13 00:41:53 +0000

    Sync LeetCode submission Runtime - 19 ms (91.23%), Memory - 32.5 MB (19.98%)

--- my-leetcode-solutions/0876-hand-of-straights/solution.cpp
+++ my-leetcode-solutions/0876-hand-of-straights/solution.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
-        int n = hand.size();
-        if (n % groupSize != 0) return false; // must be divisible
+        if((hand.size()%groupSize) != 0) return false;
 
-        map<int,int> freq;
-        // Build frequency map
-        for (int card : hand) {
-            freq[card]++;
+
+        unordered_map<int, int> count;
+        for(int card : hand) count[card]++;     
+
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        for (auto &p : count) {
+            minHeap.push(p.first);
         }
-
-        // Process groups
-        while (!freq.empty()) {
-            int start = freq.begin()->first; // smallest card
-
-            // Try to build group [start, start+1, ..., start+groupSize-1]
+       
+        while (!minHeap.empty()) {
+            int first = minHeap.top(); // smallest available card
             for (int i = 0; i < groupSize; i++) {
-                int card = start + i;
-                if (freq.find(card) == freq.end()) {
-                    return false; // missing card
-                }
-                freq[card]--;
-                if (freq[card] == 0) {
-                    freq.erase(card); // remove exhausted card
+                int card = first + i;
+                if (count.find(card) == count.end()) 
+                    return false; // missing consecutive card
+
+                count[card]--;
+                if (count[card] == 0) {
+                    count.erase(card);
+                    if (!minHeap.empty() && minHeap.top() == card)
+                        minHeap.pop(); // remove used card from heap
                 }
             }
         }
 
         return true;
+
+        
     }
 };
