e552f8ad7f5f42dc5cb8cbea915d5c4614b4a1f4
AksTraL <akshat25chauhan.2005@gmail.com>
2025-10-05 14:03:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (13.78%)

---
+++ my-leetcode-solutions/0069-sqrtx/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 0 || x == 1) return x;
+
+        long long low = 0;
+        long long high = x;
+        long long ans = 0;
+
+        while(low <= high){
+            long long mid = low + (high - low)/2;
+            long long sq = mid * mid;
+
+            if(sq == x) return mid;       
+            else if(sq < x){
+                ans = mid;                // mid is a candidate
+                low = mid + 1;
+            } else{
+                high = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
+
