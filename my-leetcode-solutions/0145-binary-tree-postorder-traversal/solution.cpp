ca7e08afe6db970bbd9da8e06363e7d81945559e
AksTraL <akshat25chauhan.2005@gmail.com>
2025-10-08 17:01:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.6 MB (6.37%)

---
+++ my-leetcode-solutions/0145-binary-tree-postorder-traversal/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        if(root==NULL) return {};
+        vector<int> tree;
+        vector<int> left = postorderTraversal(root->left);
+        vector<int> right = postorderTraversal(root->right);
+        tree.insert(tree.end(), left.begin(),left.end());
+        tree.insert(tree.end(), right.begin(),right.end());
+        tree.push_back(root->val);
+
+
+        return tree;
+
+    }
+};
