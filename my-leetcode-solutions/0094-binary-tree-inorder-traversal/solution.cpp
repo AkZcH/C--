d811d661092971240bf522c0dc044d66ef37c2a2
AksTraL <akshat25chauhan.2005@gmail.com>
2025-10-08 16:49:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.9 MB (9.57%)

--- my-leetcode-solutions/0094-binary-tree-inorder-traversal/solution.cpp
+++ my-leetcode-solutions/0094-binary-tree-inorder-traversal/solution.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-
-    void helper(TreeNode* root, vector<int>& tree){
-        if(root == nullptr){
-            return;
-        }
-
-        helper(root->left, tree);
-        tree.push_back(root->val);
-        helper(root->right, tree);
-    }
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> tree;
-        helper(root, tree);
-        return tree;
+
+        if (!root) return {};  
+
+        vector<int> left = inorderTraversal(root->left);
+        left.push_back(root->val);
+        vector<int> right = inorderTraversal(root->right);
+
+        left.insert(left.end(), right.begin(), right.end());
+
+        return left;
     }
 };
