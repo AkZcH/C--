a8bb78477e1b6d9e413fa80985956c4645a55ace
AksTraL <akshat25chauhan.2005@gmail.com>
2025-10-12 05:38:58 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11 MB (15.77%)

--- my-leetcode-solutions/0144-binary-tree-preorder-traversal/solution.cpp
+++ my-leetcode-solutions/0144-binary-tree-preorder-traversal/solution.cpp
@@ -1,33 +1,37 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
+        // vector<int> tree;
+
+        // if(root== NULL){
+        //     return tree;
+        // } 
+
+        // else{
+        //     tree.push_back(root->val);
+        //     vector<int> left = preorderTraversal(root->left);
+        //     vector<int> right = preorderTraversal(root->right);
+
+        //     tree.insert(tree.end(), left.begin(), left.end());
+        //     tree.insert(tree.end(), right.begin(), right.end());
+        // }
+
+        // return tree;
+
+        if(root==NULL) return {};
+
+        stack<TreeNode*> st;
         vector<int> tree;
 
-        if(root== NULL){
-            return tree;
-        } 
+        st.push(root);
 
-        else{
-            tree.push_back(root->val);
-            vector<int> left = preorderTraversal(root->left);
-            vector<int> right = preorderTraversal(root->right);
-
-            tree.insert(tree.end(), left.begin(), left.end());
-            tree.insert(tree.end(), right.begin(), right.end());
+        while(!st.empty()){
+            TreeNode* node = st.top();
+            tree.push_back(node->val);
+            st.pop();
+            if(node->right) st.push(node->right);
+            if(node->left) st.push(node->left);
         }
-
         return tree;
-
     }
 };
