b20f667dfa59f84f7257976c07d9aac77a909a86
AksTraL <akshat25chauhan.2005@gmail.com>
2025-10-28 02:50:49 +0000

    Sync LeetCode submission Runtime - 2 ms (72.15%), Memory - 7.9 MB (25.64%)

--- my-leetcode-solutions/1013-fibonacci-number/solution.c
+++ my-leetcode-solutions/1013-fibonacci-number/solution.c
@@ -1,18 +1,34 @@
 
 
 int fib(int n){
-    int first = 0;
-    int second = 1;
+    // int first = 0;
+    // int second = 1;
 
-    if(n==0){
-        return first;
+    // if(n==0){
+    //     return first;
+    // }
+
+    // else if(n==1){
+    //     return second;
+    // }
+
+    // else{
+    //     return fib(n-1) + fib(n-2);
+    // }
+
+        if(n==0 || n==1) return n;
+
+
+    int prev1= 1;
+    int prev2 = 0;
+    int current;
+
+
+    for(int i=2; i<=n; i++){
+        current = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = current;
     }
 
-    else if(n==1){
-        return second;
-    }
-
-    else{
-        return fib(n-1) + fib(n-2);
-    }
+    return current;
 }
