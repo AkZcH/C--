c63be343ea1dc786d8a21b1945f94d2f3f2227da
AksTraL <akshat25chauhan.2005@gmail.com>
2026-01-10 08:19:37 +0000

    Sync LeetCode submission Runtime - 3 ms (3.07%), Memory - 12.4 MB (7.84%)

--- my-leetcode-solutions/0094-binary-tree-inorder-traversal/solution.cpp
+++ my-leetcode-solutions/0094-binary-tree-inorder-traversal/solution.cpp
@@ -1,37 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-
-        // if (!root) return {};  
-        // vector<int> tree;
-        // vector<int> left = inorderTraversal(root->left);
-        // tree.insert(tree.end(), left.begin(), left.end());
-        // tree.push_back(root->val);
-        // vector<int> right = inorderTraversal(root->right);
-        // tree.insert(tree.end(), right.begin(), right.end());
-
-        // return tree;
-
-        // if(root==NULL) return {};
-
+        if(root == nullptr) return {};
         vector<int> tree;
-        stack<TreeNode*> st;
-        TreeNode* node = root;
-
-        while(!st.empty() ||(node != NULL)){
-            while(node != NULL){
-                st.push(node);
-                node = node->left;
-            }
-            
-            node = st.top();
-            st.pop();
-            tree.push_back(node->val);
-
-            node = node -> right;
-        }
-
+        vector<int> left = inorderTraversal(root->left);
+        tree.insert(tree.end(), left.begin(), left.end());
+        tree.push_back(root->val);
+        vector<int> right = inorderTraversal(root->right);
+        tree.insert(tree.end(), right.begin(), right.end());
         return tree;
     }
 };
-
