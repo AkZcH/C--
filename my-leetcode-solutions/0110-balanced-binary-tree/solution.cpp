84663fadd7119b4a0cb2065c0a8b8313f5bde069
AksTraL <akshat25chauhan.2005@gmail.com>
2026-01-10 09:28:40 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 22.9 MB (97.34%)

--- my-leetcode-solutions/0110-balanced-binary-tree/solution.cpp
+++ my-leetcode-solutions/0110-balanced-binary-tree/solution.cpp
@@ -1,35 +1,18 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if(root == NULL) return 0;
+        if(root==nullptr) return 0;
+        int left = maxDepth(root->left);
+        if(left == -1) return -1;
+        int right = maxDepth(root->right);
+        if(right == -1) return -1;
+        if (abs(left - right) > 1) return -1;
 
-        int depth=0;
-        TreeNode* node = root;
-
-        int lh = maxDepth(node->left);
-        int lr = maxDepth(node->right);
-        depth = 1 + max(lh, lr);
-
-        return depth;
+        return 1 + max(left, right);
     }
 
     bool isBalanced(TreeNode* root) {
-        if(root == NULL) return true;
-
-        int lh = 1 + maxDepth(root->left);
-        int rh = 1 + maxDepth(root->right);
-
-        return abs(lh - rh) <=1 && isBalanced(root->left) && isBalanced(root->right);
+        if(root==nullptr) return true;
+        return maxDepth(root) != -1;
     }
 };
