6f448cf201c3a486f95870aa750cfc6b6df983aa
AksTraL <akshat25chauhan.2005@gmail.com>
2026-02-10 09:11:26 +0000

    Sync LeetCode submission Runtime - 3 ms (61.85%), Memory - 11.2 MB (35.78%)

---
+++ my-leetcode-solutions/1197-parsing-a-boolean-expression/solution.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    bool parseBoolExpr(string expression) {
+        stack<char> ch;
+        stack<char> op;
+        vector<char> ev;
+
+        for(int i=0; i<expression.size(); i++){
+            if(expression[i] == '(') ch.push('m');
+            else if(expression[i] == '&' || expression[i] == '|' || expression[i] == '!' ){
+                op.push(expression[i]);
+            }
+            else if(expression[i] == ',') continue;
+            else if(expression[i] == ')'){
+                if(op.top() != '!'){
+                    char result = (op.top() == '&') ? 't' : 'f'; 
+                    while(ch.top() != 'm'){
+                        ev.push_back(ch.top());
+                        ch.pop();
+                    }
+                    ch.pop();
+                    char oper = op.top();
+                    op.pop();
+                    if(oper == '&'){
+                        for(int j=0; j<ev.size(); j++) if(ev[j] == 'f'){
+                            result = 'f';
+                            break;
+                        }
+                    }
+                    else if(oper == '|'){
+                        for(int j=0; j<ev.size(); j++) if(ev[j] == 't'){
+                            result = 't';
+                            break;
+                        } 
+                    }
+                    ch.push(result);
+                    ev.clear();
+                }
+                else{
+                    op.pop();
+                    char val = ch.top();
+                    ch.pop();
+                    ch.pop();
+                    ch.push(val == 't' ? 'f' : 't' );
+                }
+                
+            }
+            else{
+                ch.push(expression[i]);
+            }
+
+        }
+
+        return ch.top() == 't';
+    }
+};
