ed5be57dada614da0dbf04d100fce3b4e813df86
AksTraL <akshat25chauhan.2005@gmail.com>
2026-02-15 14:05:50 +0530

    pushed on 15Feb

---
+++ Topics/Graphs/01matrix.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int rows = mat.size();
+        int cols = mat[0].size();
+
+        vector<vector<int>> res(rows, vector<int>(cols, -1));
+        queue<pair<int, int>> q;
+
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                if(mat[i][j] == 0){
+                    q.push({i, j});
+                    res[i][j] = 0;
+                }
+            }
+        }
+
+        vector<int> mr = {-1, 1, 0, 0};
+        vector<int> mc = {0, 0, -1, 1};
+
+        while(!q.empty()){
+            auto [r, c] = q.front();
+            q.pop();
+
+            for(int i=0; i<4; i++){
+                    int nr = r + mr[i];
+                    int nc = c + mc[i]; 
+            
+                if(nr>=0 && nr<rows && nc>=0 && nc<cols && res[nr][nc] == -1){
+                    res[nr][nc] = res[r][c] + 1;
+                    q.push({nr, nc});
+                }
+            }
+        }
+
+        return res;
+    }
+};
