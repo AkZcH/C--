eb03427ffbab9ad356922c40f4930b8746a3c7eb
AksTraL <akshat25chauhan.2005@gmail.com>
2026-02-17 14:23:54 +0000

    Sync LeetCode submission Runtime - 44 ms (37.82%), Memory - 49.3 MB (32.03%)

--- my-leetcode-solutions/0322-coin-change/solution.cpp
+++ my-leetcode-solutions/0322-coin-change/solution.cpp
@@ -1,55 +1,22 @@
 class Solution {
 public:
-    int f(int index, int amount, vector<int>& nums){
-        int n = nums.size();
-        vector<int> prev(amount+1, 1e9);
-        vector<int> curr(amount+1, 1e9);
+    int f(vector<int>& coins, int amount, int index, vector<vector<int>>& dp){
+        if(index == 0) return (amount%coins[0]) == 0 ? amount/coins[0] : 1e9;
+        if(amount == 0) return 0;
+        if(dp[index][amount] != -1) return dp[index][amount];
 
-        prev[0]=curr[0]=0;
-        for(int j=1; j<=amount; j++){
-            if(nums[0]<=j && j%nums[0]==0) prev[j]=j/nums[0];
+        int notTake = f(coins, amount, index-1, dp);
+        int take = 1e9;
+        if(amount>=coins[index]){
+            take = 1+ f(coins, amount - coins[index], index, dp);
         }
-
-        for(int i=1; i<n;i++){
-            for(int j=1; j<=amount; j++){
-                int notTake = prev[j];
-
-                int Take = 1e9;
-                if(nums[i]<=j){
-                    Take = 1 + curr[j-nums[i]];
-                } 
-                curr[j]= min(Take, notTake);
-            }
-            prev=curr;
-        }
-
-        return prev[amount];
+        return dp[index][amount] = min(take, notTake);
     }
 
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        // vector<vector<int>> dp(n, vector<int>(amount+1, 1e9));
-
-        // for(int i=0; i<n; i++) dp[i][0] = 0;
-        // for(int j=1; j<=amount; j++){
-        //     if(coins[0]<=j && j%coins[0]==0) dp[0][j]= j/coins[0];
-        // }
-
-        // for(int i=1; i<n;i++){
-        //     for(int j=1; j<=amount; j++){
-        //         int notTake = dp[i-1][j];
-
-        //         int Take = 1e9;
-        //         if(coins[i]<=j){
-        //             Take = 1 + dp[i][j-coins[i]];
-        //         } 
-        //         dp[i][j]= min(Take, notTake);
-        //     }
-        // }
-
-        int ans = f(n, amount, coins);
-        return ans >= 1e9 ? -1 : ans;
-
-        // return dp[n-1][amount] >= 1e9 ? -1 : dp[n-1][amount];
+        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        int ans = f(coins, amount, n-1, dp);
+        return ans == 1e9 ? -1 : ans;
     }
 };
