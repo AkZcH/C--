d4345216f9cf062ce4a3c34e955607961cf40640
AksTraL <akshat25chauhan.2005@gmail.com>
2026-02-24 11:55:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.8 MB (11.89%)

--- my-leetcode-solutions/0027-remove-element/solution.cpp
+++ my-leetcode-solutions/0027-remove-element/solution.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int res=0;
+        int n = nums.size();
+        int write = 0;
 
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]!=val){
-                nums[res]=nums[i];
-                res++;
-            }
+        for(int i=0; i<n; i++){
+            if(nums[i] != val) nums[write++] = nums[i];
         }
-        return res;
+
+        return write;
     }
 };
