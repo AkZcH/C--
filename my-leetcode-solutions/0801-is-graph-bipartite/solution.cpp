7cd0e312efe9aee35d54adc5ae64a0aca6f42031
AksTraL <akshat25chauhan.2005@gmail.com>
2026-03-21 01:40:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.2 MB (98.51%)

--- my-leetcode-solutions/0801-is-graph-bipartite/solution.cpp
+++ my-leetcode-solutions/0801-is-graph-bipartite/solution.cpp
@@ -1,27 +1,29 @@
 class Solution {
 public:
-
-    bool dfs(int node, int col, vector<int>& color, vector<vector<int>>& graph){
-        color[node] = col;
+    bool dfs(int node, int color, vector<vector<int>>& graph, vector<int>& colored){
+        colored[node] = color;
 
         for(auto it : graph[node]){
-            if(color[it] == -1){
-                if(dfs(it, !col, color, graph) == false) return false;
+            if(colored[it] == -1){
+                if(dfs(it, !color, graph, colored) == false) return false;
             }
-            else if(color[it] == col) return false;
+            else if(colored[it] == color){
+                return false;
+            }
         }
         return true;
     }
 
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> color(n, -1);
+        vector<int> colored(n, -1);
 
-        for(int i=0; i<n;  i++){
-            if(color[i] == -1){
-                if(dfs(i, 0, color, graph) == false) return false;
+        for(int i=0; i<n; i++){
+            if(colored[i] == -1){
+                if(dfs(i, 0, graph, colored) == false) return false;
             }
         }
+
         return true;
     }
 };
